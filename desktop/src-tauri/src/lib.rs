7a1405dd20ae0aed1351136c73ece0aaeebf95df
Nikhil <nikhil181245@gmail.com>
2026-05-27 15:01:18 +0530

    feat: re-add auto-updater with proper signing keys

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -445,6 +445,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .register_uri_scheme_protocol("cozy", |_app, request| {
